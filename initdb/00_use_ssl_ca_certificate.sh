c8a1be3b7992663e8c046f63b42af346a8198053
Joel Bernstein <joel@fysh.org>
2018-03-01 19:36:00 +0100

    more robust ssl cert handling

--- initdb/00_use_ssl_ca_certificate.sh
+++ initdb/00_use_ssl_ca_certificate.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+
 if [ -n "${DB_SSL_CA_CERT}" ]; then
-  export DB_SSL=mysql-ca.pem
-  echo "${DB_SSL_CA_CERT}" > ${DB_SSL}
+  >&2 echo "found SSL certificate in DB_SSL_CA_CERT, using that for the Mariadb CA"
+  export DB_SSL=/mysql-ca.pem
+  if [ -e ${DB_SSL} ]; then
+    echo "File ${DB_SSL} already exists, ignoring DB_SSL_CA_CERT."
+  else
+    echo "${DB_SSL_CA_CERT}" | sed -e 's,\\n,\n,g' > ${DB_SSL}
+  fi
 fi
